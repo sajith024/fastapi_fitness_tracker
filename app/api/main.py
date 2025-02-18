f4b931e021b74572f83571f64422c111b69064d1
sajith024 <164509417+sajith024@users.noreply.github.com>
2025-02-18 18:04:29 +0530

    added goals api

--- app/api/main.py
+++ app/api/main.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from .routes import auth, workouts, utils
+from .routes import auth, goals, workouts, utils
 
 api_router = APIRouter()
 api_router.include_router(auth.router)
+api_router.include_router(goals.router)
 api_router.include_router(workouts.router)
 api_router.include_router(utils.router)
